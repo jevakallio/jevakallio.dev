777a1333a473be9298bfd9ed300e519de79c1b59
Jani Eväkallio <jani.evakallio@gmail.com>
2019-12-12 16:36:56 +0000

    Fancify sidebar (maybe too much, todo fix)

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,53 @@
-var baseHue = +getComputedStyle(document.documentElement).getPropertyValue(
-  "--primary-hue"
-);
-
-window.addEventListener("scroll", function() {
-  requestAnimationFrame(function() {
-    const progress = window.scrollY; // window.document.body.clientHeight;
-    setPrimaryColorBasedOnScrollPosition(progress);
-  });
-});
-
-function setPrimaryColorBasedOnScrollPosition(progress) {
-  document.documentElement.style.setProperty(
-    "--primary-hue",
-    baseHue + progress / 30
+window.addEventListener("DOMContentLoaded", function() {
+  var baseHue = +getComputedStyle(document.documentElement).getPropertyValue(
+    "--primary-hue"
   );
-}
+
+  window.addEventListener("scroll", function() {
+    requestAnimationFrame(function() {
+      const progress = window.scrollY; // window.document.body.clientHeight;
+      setPrimaryColorBasedOnScrollPosition(progress);
+    });
+  });
+
+  var intersectionObserver = new IntersectionObserver(
+    entries => {
+      if (entries.length === 1) {
+        const id = entries[0].target.id;
+        if (id) {
+          const hash = `#${id}`;
+          history.replaceState(null, null);
+          updateActiveSidebarItem(hash);
+        }
+      }
+    },
+    {
+      rootMargin: `0px 0px -${window.innerHeight - 130}px 0px`,
+      threshold: 1
+    }
+  );
+
+  document.querySelectorAll("h1, .content h2").forEach(h => {
+    intersectionObserver.observe(h);
+  });
+
+  window.addEventListener("hashchange", () => {
+    updateActiveSidebarItem(window.location.hash);
+  });
+
+  function updateActiveSidebarItem(hash) {
+    document.querySelectorAll(".toc a").forEach(e => {
+      if (hash && e.href.endsWith(hash)) {
+        e.classList.add("active");
+      } else if (e.classList.contains("active")) {
+        e.classList.remove("active");
+      }
+    });
+  }
+  function setPrimaryColorBasedOnScrollPosition(progress) {
+    document.documentElement.style.setProperty(
+      "--primary-hue",
+      baseHue + progress / 30
+    );
+  }
+});
